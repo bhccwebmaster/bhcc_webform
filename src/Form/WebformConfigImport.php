254997e516681d6eba3707bb53aad732391518aa
Hannah Harvey <hannah.harvey@brighton-hove.gov.uk>
2024-07-30 13:19:08 +0100

    Fix code standard issues

--- src/Form/WebformConfigImport.php
+++ src/Form/WebformConfigImport.php
@@ -6,7 +6,6 @@ namespace Drupal\bhcc_webform\Form;
 
 use Drupal\config\Form\ConfigSingleImportForm;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\webform\WebformInterface;
 
 /**
  * Provides a BHCC Webform form.
@@ -45,12 +44,12 @@ final class WebformConfigImport extends ConfigSingleImportForm {
    */
   public function validateForm(array &$form, FormStateInterface $form_state): void {
     parent::validateForm($form, $form_state);
-    
+
     $import_data = $form_state->getValue('import');
 
     if (!empty($import_data)) {
       $definition = $this->entityTypeManager->getDefinition($form_state->getValue('config_type'));
-      
+
       $valid_config = [
         'webform',
         'webform_option',
