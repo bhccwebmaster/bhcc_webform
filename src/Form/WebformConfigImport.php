dc2b9116994a4a7355490687701550165642095e
Hannah Harvey <hannah.harvey@brighton-hove.gov.uk>
2024-08-05 15:08:28 +0100

    Add comments to explain what is happening.

--- src/Form/WebformConfigImport.php
+++ src/Form/WebformConfigImport.php
@@ -7,6 +7,7 @@ namespace Drupal\bhcc_webform\Form;
 use Drupal\config\Form\ConfigSingleImportForm;
 use Drupal\Core\Form\FormStateInterface;
 
+
 /**
  * Provides a BHCC Webform form.
  */
@@ -16,6 +17,7 @@ final class WebformConfigImport extends ConfigSingleImportForm {
    * {@inheritdoc}
    */
   public function getFormId(): string {
+    // Return the unique ID for this form.
     return 'bhcc_webform_webform_config_import';
   }
 
@@ -23,6 +25,7 @@ final class WebformConfigImport extends ConfigSingleImportForm {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
+
 
     $form = parent::buildForm($form, $form_state);
 
@@ -35,7 +38,6 @@ final class WebformConfigImport extends ConfigSingleImportForm {
     if (isset($form['config_type'])) {
       $form['config_type']['#options'] = $configuration_type_options;
     }
-
     return $form;
   }
 
@@ -43,11 +45,21 @@ final class WebformConfigImport extends ConfigSingleImportForm {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state): void {
+    
     parent::submitForm($form, $form_state);
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
-    // Redirect back to importer form.
-    // @todo Try to redirect to form build page.
-    $form_state->setRedirect('bhcc_webform.webform_config_import');
-  }
 
+    if (!empty($this->data['import'])) {
+      $import = $this->data['import'];
+      $webform_id = $import['id'];
+
+    // Determine the redirection route along with the specified parameters.
+    // Route name is the redirect to the import form builder page.
+    // Parameters are the webform ids relating to the redirect.
+    $route_name = 'entity.webform.edit_form';
+    $parameters = ['webform' => $webform_id];
+
+    // Set a redirect response after the form has been submitted.
+    $form_state->setRedirect($route_name, $parameters);
+    }
+  }
 }
