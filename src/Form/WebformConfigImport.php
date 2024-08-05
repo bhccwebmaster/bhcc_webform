cf4e7401cc2b7e35b49ff5112357b771755e1a08
Rose Nichols <rose.nichols@brighton-hove.gov.uk>
2024-08-05 17:06:22 +0100

    Wire up webform_options redirect

--- src/Form/WebformConfigImport.php
+++ src/Form/WebformConfigImport.php
@@ -7,7 +7,6 @@ namespace Drupal\bhcc_webform\Form;
 use Drupal\config\Form\ConfigSingleImportForm;
 use Drupal\Core\Form\FormStateInterface;
 
-
 /**
  * Provides a BHCC Webform form.
  */
@@ -25,7 +24,6 @@ final class WebformConfigImport extends ConfigSingleImportForm {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-
 
     $form = parent::buildForm($form, $form_state);
 
@@ -49,27 +47,28 @@ final class WebformConfigImport extends ConfigSingleImportForm {
     parent::submitForm($form, $form_state);
 
     if (!empty($this->data['import'])) {
+
       $import = $this->data['import'];
-      $webform_id = $import['id'];
+      $id = $import['id'];
 
       // Determine the redirection route along with the specified parameters
       // based on whether we're importing webform config or webform_options.
-      if (config_type = 'webform') {
+      if ($this->data['config_type'] == 'webform') {
         // Route name is the redirect to the import form builder page.
         // Parameters are the webform ids relating to the redirect.
         $route_name = 'entity.webform.edit_form';
-        $parameters = ['webform' => $webform_id];
+        $parameters = ['webform' => $id];
       }
       else {
         // Route name is the redirect to the options edit page.
         // Parameters are the webform option id.
-
-        $route_name = '';
-        $parameters = [];
+        $route_name = 'entity.webform_options.edit_form';
+        $parameters = ['webform_options' => $id];
       }
 
       // Set a redirect response after the form has been submitted.
       $form_state->setRedirect($route_name, $parameters);
     }
   }
+
 }
